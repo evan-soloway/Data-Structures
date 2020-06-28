7410a9a3cc9e740580560abf870c07890320c3e6
Evan Soloway <52336842+evan-soloway@users.noreply.github.com>
2020-06-28 05:33:52 -0500

    temp adding of debug files

---
+++ debug.h
@@ -0,0 +1,22 @@
+/* PseudoCode outline
+
+- Must include example of inheritance
+- Must include instance of method override
+
+Base class(es): class ECF, class ELO
+Inheriting class(es): class Player
+
+Override instance: 
+
+ECF::getRating(){algorithm}
+ELO::getRating(){algorithm}
+Player::getRating(ECF object1, ELO object2){object1.getRating() || object2.getRating}
+
+-----
+
+Program Order (logical hierarchy):
+ECF.h -> ECF.cpp -> ELO.h -> ELO.cpp -> Player.h -> Player.cpp -> main.h -> main.cpp
+
+
+*/
+
